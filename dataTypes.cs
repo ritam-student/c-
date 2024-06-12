bb868631cb118bcbf4d03a07771c952170d167ba
Ritam Acharya <ritamacharya2004@gmail.com>
2024-06-12 19:45:53 +0530

    add initial project files

---
+++ dataTypes.cs
@@ -0,0 +1,25 @@
+
+
+// combine string using escape sequence character
+using System.Net.Security;
+/***
+string name1 = "ram";
+string name2 = "roy";
+Console.WriteLine(name1 + " " + name2);
+Console.WriteLine("Hello\nworld...");
+Console.WriteLine("Hello\tworld...");
+Console.WriteLine("hello \"ritam\" hi! ");
+Console.WriteLine("c:\\user\\Lenovo\\oneDrive\\Desktop\\c#>");
+
+Console.WriteLine("Generating invoice for customer \"Contoso crop\" ....");
+Console.WriteLine();
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine();
+Console.WriteLine("Output Directory");
+*/
+
+// verbatim string literal
+Console.WriteLine(@"    c:\\source\\repos
+            (this is where your code goes)");
+Console.WriteLine("\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a");
